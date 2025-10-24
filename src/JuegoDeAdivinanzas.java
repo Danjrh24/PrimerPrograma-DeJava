58e1f2cb593eabe97b7cdef75c4cf1f2e1c75945
Daniel Rodriguez <daniel.rh2407@gmail.com>
2025-10-23 20:49:17 -0400

    agragando ciclo while para jugar varias veces

--- src/JuegoDeAdivinanzas.java
+++ src/JuegoDeAdivinanzas.java
@@ -4,25 +4,43 @@ import java.util.Scanner;
 public class JuegoDeAdivinanzas {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        int numeroAleatorio = new Random().nextInt(100);
-        int numeroUsuario = 0;
-        System.out.println("\n ======Bienvenido al super juego de adivinar el numero====== \n");
-        for (int i = 0; i < 5 ; i++) {
-            System.out.println("Intenta adivinar el numero, ingresa uno del 1 al 100...");
-            numeroUsuario = userInput.nextInt();
-            if(numeroUsuario == numeroAleatorio) {
-                System.out.println("Felicidades, adivinaste el numero");
-                break;
-            } else if (numeroUsuario > numeroAleatorio) {
-                System.out.println("El numero ingresado es mayor");
-            } else if (numeroUsuario < numeroAleatorio) {
-                System.out.println("El numero ingresado es menor");
+        int eleccionJugador = 0;
+        boolean jugadorQuiereJugar = true;
+        System.out.println("""
+                
+                ======Bienvenido al super juego de adivinar el numero======
+                =============Tienes 5 intentos para ganar============
+                """);
+        while (jugadorQuiereJugar) {
+            int numeroAleatorio = new Random().nextInt(100);
+            int numeroUsuario = 0;
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Intenta adivinar el numero, ingresa uno del 1 al 100...");
+                numeroUsuario = userInput.nextInt();
+                if (numeroUsuario == numeroAleatorio) {
+                    System.out.println("Felicidades, adivinaste el numero");
+                    break;
+                } else if (numeroUsuario > numeroAleatorio) {
+                    System.out.println("El numero ingresado es mayor");
+                } else if (numeroUsuario < numeroAleatorio) {
+                    System.out.println("El numero ingresado es menor");
+                }
+                System.out.println("Intento numero " + (i + 1));
+                if (i == 4) {
+                    System.out.println("""
+                            Lo siento, no pudiste adivinar el numero
+                            Deseas continuar? 
+                            1. Si 
+                            2. No
+                            """);
+                    eleccionJugador = userInput.nextInt();
+                    if (eleccionJugador == 2) {
+                        System.out.println("Gracias por jugar");
+                        jugadorQuiereJugar = false;
+                        break;
+                    }
+                }
             }
-            if (i == 4) {
-                System.out.println("Lo siento, no pudiste adivinar el numero");
-                break;
-            }
-            System.out.println("Intento numero " + (i + 1));
         }
     }
 }
